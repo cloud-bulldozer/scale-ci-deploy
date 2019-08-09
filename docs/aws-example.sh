8286afd3aae49cb3eab36fe861c8039711fd74f8
akrzos <akrzos@redhat.com>
2019-08-09 16:30:10 -0400

    Add documentation for all OCP4 environments to be installed from.

---
+++ docs/aws-example.sh
@@ -0,0 +1,89 @@
+#!/usr/bin/env bash
+# Machine to use in inventory file
+export ORCHESTRATION_HOST="localhost"
+
+export PUBLIC_KEY=~/.ssh/id_rsa.pub
+export PRIVATE_KEY=~/.ssh/id_rsa
+
+export ORCHESTRATION_USER=root
+
+export OPENSHIFT_CLEANUP=false
+export OPENSHIFT_INSTALL=true
+export OPENSHIFT_POST_INSTALL=true
+export OPENSHIFT_POST_CONFIG=true
+export OPENSHIFT_DEBUG_CONFIG=false
+
+export OPENSHIFT_CLIENT_LOCATION="https://mirror.openshift.com/pub/openshift-v4/clients/ocp/4.1.9/openshift-client-linux-4.1.9.tar.gz"
+export OPENSHIFT_INSTALL_RELEASE_IMAGE_OVERRIDE=
+export OPENSHIFT_INSTALL_APIVERSION=v1
+export OPENSHIFT_INSTALL_SSH_PUB_KEY_FILE=~/.ssh/id_rsa.pub
+export OPENSHIFT_INSTALL_PULL_SECRET=
+export OPENSHIFT_INSTALL_QUAY_REGISTRY_TOKEN=
+export OPENSHIFT_INSTALL_IMAGE_REGISTRY=registry.svc.ci.openshift.org
+export OPENSHIFT_INSTALL_REGISTRY_TOKEN=
+export OPENSHIFT_INSTALL_INSTALLER_FROM_SOURCE=false
+export OPENSHIFT_INSTALL_INSTALLER_FROM_SOURCE_VERSION=master
+export GOPATH=/root/.go
+
+export AWS_PROFILE=
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_REGION=
+
+export OPENSHIFT_BASE_DOMAIN=
+export OPENSHIFT_CLUSTER_NAME=testcluster0
+export OPENSHIFT_MASTER_COUNT=3
+export OPENSHIFT_WORKER_COUNT=5
+export OPENSHIFT_MASTER_INSTANCE_TYPE=m5.xlarge
+export OPENSHIFT_WORKER_INSTANCE_TYPE=m5.large
+export OPENSHIFT_MASTER_ROOT_VOLUME_SIZE=64
+export OPENSHIFT_MASTER_ROOT_VOLUME_TYPE=gp2
+export OPENSHIFT_MASTER_ROOT_VOLUME_IOPS=0
+export OPENSHIFT_WORKER_ROOT_VOLUME_SIZE=64
+export OPENSHIFT_WORKER_ROOT_VOLUME_TYPE=gp2
+export OPENSHIFT_WORKER_ROOT_VOLUME_IOPS=0
+export OPENSHIFT_CIDR=10.128.0.0/14
+export OPENSHIFT_MACHINE_CIDR=10.0.0.0/16
+export OPENSHIFT_SERVICE_NETWORK=172.30.0.0/16
+export OPENSHIFT_HOST_PREFIX=23
+
+export OPENSHIFT_POST_INSTALL_POLL_ATTEMPTS=600
+export OPENSHIFT_TOGGLE_INFRA_NODE=true
+export OPENSHIFT_TOGGLE_WORKLOAD_NODE=true
+## Either machine.openshift.io or sigs.k8s.io
+export MACHINESET_METADATA_LABEL_PREFIX=machine.openshift.io
+export OPENSHIFT_INFRA_NODE_INSTANCE_TYPE=m5.large
+export OPENSHIFT_WORKLOAD_NODE_INSTANCE_TYPE=m5.large
+export OPENSHIFT_INFRA_NODE_VOLUME_SIZE=64
+export OPENSHIFT_INFRA_NODE_VOLUME_TYPE=gp2
+export OPENSHIFT_INFRA_NODE_VOLUME_IOPS=0
+export OPENSHIFT_WORKLOAD_NODE_VOLUME_SIZE=64
+export OPENSHIFT_WORKLOAD_NODE_VOLUME_TYPE=gp2
+export OPENSHIFT_WORKLOAD_NODE_VOLUME_IOPS=0
+export OPENSHIFT_PROMETHEUS_RETENTION_PERIOD=15d
+export OPENSHIFT_PROMETHEUS_STORAGE_CLASS=gp2
+export OPENSHIFT_PROMETHEUS_STORAGE_SIZE=10Gi
+export OPENSHIFT_ALERTMANAGER_STORAGE_CLASS=gp2
+export OPENSHIFT_ALERTMANAGER_STORAGE_SIZE=2Gi
+
+export KUBECONFIG_AUTH_DIR_PATH=
+
+# Clean up old kubeconfig
+rm -rf OCP-4.X/kubeconfig
+
+####################################################################################################
+# Start Jenkins Build:
+####################################################################################################
+# List the environment variables.
+set -o pipefail
+set -eux
+
+echo "[orchestration]" > inventory
+echo "${ORCHESTRATION_HOST}" >> inventory
+
+ansible-playbook -vv -i inventory OCP-4.X/install-on-aws.yml
+
+####################################################################################################
+# Post Jenkins Build Clean up:
+####################################################################################################
+rm -rf inventory
---
+++ docs/ocp4_scale.md
@@ -0,0 +1,40 @@
+# OpenShift 4 Scale playbook
+
+Currently the OpenShift 4 scale playbook has only been tested against environments in AWS on us-west-2 region. The playbook follows the same conventions as the install playbooks in terms of running/orchestrating.
+
+The scale playbook is `OCP-4.X/scale.yml` and can scale an existing cluster.
+
+Running from CLI:
+
+```sh
+$ cp OCP-4.X/inventory.example inventory
+$ # Add orchestration host to inventory
+$ # Edit vars in OCP-4.X/vars/scale.yml or define Environment vars (See below)
+$ ansible-playbook -vv -i inventory OCP-4.X/scale.yml
+```
+
+## Environment variables
+
+### PUBLIC_KEY
+Default: `~/.ssh/id_rsa.pub`  
+Public ssh key file for Ansible.
+
+### PRIVATE_KEY
+Default: `~/.ssh/id_rsa`  
+Private ssh key file for Ansible.
+
+### ORCHESTRATION_USER
+Default: `root`  
+User for Ansible to log in as. Must authenticate with PUBLIC_KEY/PRIVATE_KEY.
+
+### POLL_ATTEMPTS
+Default: `600`  
+The number of times for the playbook to poll the OpenShift cluster to determine if the worker count has scaled as expected.
+
+### RHCOS_METADATA_LABEL_PREFIX
+Default: `machine.openshift.io`  
+The prefix used in machinesets. Usually this is `machine.openshift.io` however it might be `sigs.k8s.io` depending on version installed.
+
+### RHCOS_WORKER_COUNT
+Default: `5`  
+The total desired count of nodes.
