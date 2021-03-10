35cb61488bbda70a46a51e86907d9950eb6a65c9
Harshith Umesh <humesh@humesh.remote.csb>
2021-03-10 11:53:31 +0530

    ci

---
+++ CI/README.md
@@ -0,0 +1,14 @@
+## Usage Example:
+
+Initialise the environment variables in bash script in CI/properties_files [here](CI/properties_files/)for the required platform to deploy the cluster, certain env variables have been initialised for a CI run, however some variables are left not populated such as AWS/Azure/GCP credentials and OpenShift pull secrets.By reviewing the script you can see how the variables are intended to be populated for CI integration.
+
+### After initialising run the below command:
+```sh
+$ ./CI/run_ci.sh platform
+```
+
+Where platform can be either `aws`, `azure` or `gcp`.
+
+The orchestration host can be `localhost` or a remote machine already provisioned on the cloud in which the cluster will be deployed on.  If executing on a local machine or from Jenkins, `localhost` should be used.
+
+
---
+++ CI/common.sh
@@ -0,0 +1,88 @@
+#! /bin/bash
+
+#common to all platforms
+
+if [[ -z $ORCHESTRATION_USER ]];then 
+    echo 'ORCHESTRATION_USER not set, exiting!'
+    exit 1
+fi
+
+if [[ -z $ORCHESTRATION_HOST ]];then 
+    echo 'ORCHESTRATION_HOST not set, exiting!'
+    exit 1 
+fi
+
+if [[ -z $OPENSHIFT_CLIENT_LOCATION ]];then 
+    echo 'OPENSHIFT_CLIENT_LOCATION not set, exiting!'
+    exit 1
+fi
+
+if [[ -z $OPENSHIFT_BASE_DOMAIN ]];then 
+    echo 'OPENSHIFT_BASE_DOMAIN not set, exiting!'
+    exit 1
+fi
+
+if [[ -z $OPENSHIFT_CLUSTER_NAME ]];then 
+    echo 'OPENSHIFT_CLUSTER_NAME not set, exiting!'
+    exit 1
+fi
+
+if [[ -n $KUBECONFIG_PATH ]]; then
+  export KUBECONFIG=$KUBECONFIG_PATH
+else
+  echo "Looks like kubeconfig path is not set, assuming that it's in the default location - $HOME/.kube/config"
+fi
+
+if [[ -z $SSHKEY_TOKEN ]];then 
+    echo 'SSHKEY_TOKEN not set, exiting!'
+    exit 1
+fi
+
+if [[ -z $OPENSHIFT_INSTALL_PULL_SECRET ]];then 
+    echo 'OPENSHIFT_INSTALL_PULL_SECRET not set, exiting!'
+    exit 1
+fi
+
+if [[ -z $OPENSHIFT_INSTALL_QUAY_REGISTRY_TOKEN ]];then 
+    echo 'OPENSHIFT_INSTALL_QUAY_REGISTRY_TOKEN not set, exiting!'
+    exit 1
+fi
+
+if [[ -z $OPENSHIFT_INSTALL_IMAGE_REGISTRY ]];then 
+    echo 'OPENSHIFT_INSTALL_IMAGE_REGISTRY not set, exiting!'
+    exit 1
+fi
+
+if [[ -z $OPENSHIFT_INSTALL_REGISTRY_TOKEN ]];then 
+    echo 'OPENSHIFT_INSTALL_REGISTRY_TOKEN not set, exiting!'
+    exit 1
+fi
+
+if [[ -z $ES_SERVER || -z $ELASTIC_CURL_USER || -z $ELASTIC_SERVER || -z $ELASTIC_CURL_URL ]]; then
+  echo 'one or more variables for elastic search are undefined, exiting!'
+  exit 1
+fi
+
+#aws
+if [[ $PLATFORM = "aws" ]];then 
+    if [[ -z $AWS_PROFILE || -z $AWS_ACCESS_KEY_ID || -z $AWS_SECRET_ACCESS_KEY || -z $AWS_REGION ]]; then
+        echo 'one or more aws credentials are undefined, exiting!'
+        exit 1
+    fi    
+fi
+
+#azure
+if [[ $PLATFORM = "azure" ]];then 
+    if [[ -z $AZURE_SUBSCRIPTION_ID || -z $AZURE_TENANT_ID || -z $AZURE_SERVICE_PRINCIPAL_CLIENT_ID || -z $AZURE_SERVICE_PRINCIPAL_CLIENT_SECRET || -z $AZURE_BASE_DOMAIN_RESOURCE_GROUP_NAME || -z $AZURE_REGION ]]; then
+        echo 'one or more azure credentials are undefined, exiting!'
+        exit 1
+    fi    
+fi
+
+#gcp
+if [[ $PLATFORM = "gcp" ]];then 
+    if [[ -z $GCP_REGION || -z $GCP_PROJECT || -z $GCP_SERVICE_ACCOUNT || -z $GCP_SERVICE_ACCOUNT_EMAIL || -z $GCP_AUTH_KEY_FILE ]]; then
+        echo 'one or more GCP credentials are undefined, exiting!'
+        exit 1
+    fi    
+fi
