571b1de90bdc1cc9f0ac4c0a7ae9b5c6cff6213c
Harshith u <44830597+Harshith-umesh@users.noreply.github.com>
2021-03-23 23:00:43 +0530

    Update run_ci.sh

--- CI/run_ci.sh
+++ CI/run_ci.sh
@@ -3,19 +3,17 @@
 set -o pipefail
 set -eu
 
-_platform=$@                                  #to accept the platform as input from command line
+export PLATFORM=$@                                  #to accept the platform as input from command line
 
-if [[ ${_platform} == '' ]]; then
+if [[ ${PLATFORM} == '' ]]; then
   echo -e "Wrong usage, please enter a platform choice: aws/azure/gcp"
   exit 1
+elif [[ ${PLATFORM} == "all" ]]; then
+  declare -a test_list=("aws" "azure" "gcp")
+else 
+  test_list=$PLATFORM
+  
 fi
-
-
-chmod +x CI/properties_files/${_platform}_openshift_installer.sh 
-source CI/properties_files/${_platform}_openshift_installer.sh       #to initialize the required env variables
-
-export PLATFORM=$_platform
-source CI/common.sh                          # to check if all environment variables are set accordingly
 
 diff_list=`git diff --name-only origin/master`
 echo -e "List of files changed : ${diff_list} \n"
@@ -23,35 +21,53 @@ echo -e "List of files changed : ${diff_list} \n"
 cat > results.markdown << EOF
 Results for scale-ci-deploy
 Platform                       | Result | Runtime  |
--------------------------------|--------|----------|
+-------------------------------|--------|----------| 
 EOF
 
-test_rc=0    
-start_time=`date`
-  
-echo -e "\n======================================================================"
-echo -e "     CI test for scale-ci-deploy in ${_platform}                    "
-echo -e "======================================================================\n"
-  
-# Create inventory File:
-echo "[orchestration]" > inventory
-echo "${ORCHESTRATION_HOST}" >> inventory
-  
-ansible-playbook -v -i inventory OCP-4.X/deploy-cluster.yml -e platform=${_platform} 
+test_rc=0  
 
-EXIT_STATUS=$?
-if [ "$EXIT_STATUS" -eq "0" ]                         #to check if the test exits successfully or not
-then
-    result="PASS"
-else
-    result="FAIL"
-    test_rc=1
-fi
+for test in ${test_list[@]}; do
 
-end_time=`date`
-duration=`date -ud@$(($(date -ud"$end_time" +%s)-$(date -ud"$start_time" +%s))) +%T`
+  chmod +x CI/properties_files/${test}_openshift_installer.sh 
+  source CI/properties_files/${test}_openshift_installer.sh       #to initialize the required env variables
+  export _PLATFORM=$test                       #needed to check the platform in common.sh
+  source CI/common.sh                          # to check if all environment variables are set accordingly
+    
+  start_time=`date`
   
-echo "${_platform}                       | ${result} | ${duration}" >> results.markdown
+  echo -e "\n======================================================================"
+  echo -e "     CI test for scale-ci-deploy in ${test}                    "
+  echo -e "======================================================================\n"
+  
+  # Create inventory File:
+  echo "[orchestration]" > inventory
+  echo "${ORCHESTRATION_HOST}" >> inventory
+
+  export ANSIBLE_FORCE_COLOR=true 
+  ansible-playbook -v -i inventory OCP-4.X/deploy-cluster.yml -e platform=${test} 
+
+  EXIT_STATUS=$?
+  if [ "$EXIT_STATUS" -eq "0" ]                         #to check if the test exits successfully or not
+  then
+      result="PASS"
+  else
+      result="FAIL"
+      test_rc=1
+  fi
+
+  end_time=`date`
+  duration=`date -ud@$(($(date -ud"$end_time" +%s)-$(date -ud"$start_time" +%s))) +%T`
+  
+  echo "${test}                            | ${result} | ${duration}" >> results.markdown
+
+  #Destroying cluster
+  cd /root/scale-ci-$OPENSHIFT_CLUSTER_NAME-$test/          
+  echo $PWD                                                
+  ./bin/openshift-install destroy cluster     # comment out this code block to prevent deleting cluster
+  cd /root/scale-ci-deploy/ 
+  echo $PWD 
+
+done 
 
 cat results.markdown
 
